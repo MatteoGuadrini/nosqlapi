e8c1ce85787d8e7e0bf44325c7e535613a84d9ef
Matteo Guadrini <matteo.guadrini@hotmail.it>
2021-07-01 10:26:13 +0200

    Added test_kvdb_close on KVConnectionTest test class

--- tests/test_kvdb.py
+++ tests/test_kvdb.py
@@ -32,11 +32,15 @@ class KVConnectionTest(unittest.TestCase):
             self.connection = self.t
 
         def create_database(self, name):
-            self.connection.send(f"CREATE_DB='{name}'".encode())
-            while len(self.connection.recv(2048)) > 0:
-                self._return_data.append(self.connection.recv(2048))
-            if self.return_data.decode() != 'DB_CREATED':
-                raise DatabaseCreationError(f'Database creation error: {self.return_data.decode()}')
+            if self.connection:
+                self.connection.send(f"CREATE_DB='{name}'".encode())
+                # while len(self.t.recv(2048)) > 0:
+                self.t.recv = mock.MagicMock(return_value='DB_CREATED')
+                self._return_data = self.t.recv(2048)
+                if self.return_data != 'DB_CREATED':
+                    raise DatabaseCreationError(f'Database creation error: {self.return_data.decode()}')
+            else:
+                raise ConnectError(f"Server isn't connected")
 
         def has_database(self, name):
             self.connection.send(f"HAS_DB='{name}'".encode())
@@ -70,6 +74,15 @@ class KVConnectionTest(unittest.TestCase):
         myconn = self.MyDBConnection('mykvdb.local', 12345)
         myconn.connect()
         self.assertEqual(myconn.return_data, 'OK_PACKET')
+        myconn.close()
+        self.assertEqual(myconn.return_data, 'CLOSED')
+
+    def test_kvdb_create_database(self):
+        myconn = self.MyDBConnection('mykvdb.local', 12345)
+        myconn.connect()
+        self.assertEqual(myconn.return_data, 'OK_PACKET')
+        myconn.create_database('test_db')
+        self.assertEqual(myconn.return_data, 'DB_CREATED')
         myconn.close()
         self.assertEqual(myconn.return_data, 'CLOSED')
 
