6e34cd7637cbb42e38ed0b8b30932a1ae5909e57
Matteo Guadrini <matteo.guadrini@hotmail.it>
2021-07-03 07:48:48 +0200

    New alpha version

--- __info__.py
+++ __info__.py
@@ -22,7 +22,7 @@
 
 """Information variable used by modules on this package."""
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = 'Matteo Guadrini'
 __email__ = 'matteo.guadrini@hotmail.it'
 __homepage__ = 'https://github.com/MatteoGuadrini/pynosql'
