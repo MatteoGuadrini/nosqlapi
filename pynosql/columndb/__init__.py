e9ddd26a63b296c05d1569ea8a12290368afe96a
Matteo Guadrini <matteo.guadrini@hotmail.it>
2021-07-12 09:00:40 +0200

    Added columndb package

--- pynosql/columndb/__init__.py
+++ pynosql/columndb/__init__.py
@@ -19,7 +19,3 @@
 #
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-"""Package key-value NOSQL database."""
-
-from pynosql.kvdb.client import KVConnection, KVSelector, KVSession, KVResponse
