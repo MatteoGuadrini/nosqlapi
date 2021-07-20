9baacac25af36ca6b9e9f3413321af4d0f5ec6c4
Matteo Guadrini <matteo.guadrini@hotmail.it>
2021-07-20 10:21:10 +0200

    Added DocConnection class on client doc module

---
+++ pynosql/docdb/client.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+# vim: se ts=4 et syn=python:
+
+# created by: matteo.guadrini
+# client -- pynosql
+#
+#     Copyright (C) 2021 Matteo Guadrini <matteo.guadrini@hotmail.it>
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Client module for column NOSQL database."""
+# region imports
+from abc import ABC
+from ..common.core import Connection
+
+
+# endregion
+
+# region classes
+class DocConnection(Connection, ABC):
+
+    def __init__(self,
+                 host=None,
+                 port=None,
+                 username=None,
+                 password=None,
+                 ssl=None,
+                 tls=None,
+                 cert=None,
+                 ca_cert=None,
+                 ca_bundle=None
+                 ):
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        self.ssl = ssl
+        self.tls = tls
+        self.cert = cert
+        self.ca_cert = ca_cert
+        self.ca_bundle = ca_bundle
+        self.connection = None
+        self._return_data = None
+
+# endregion
