1ae156a9e515737f94e99ef0adca08157c1f5d12
Matteo Guadrini <matteo.guadrini@hotmail.it>
2021-07-21 11:00:25 +0200

    Added test_docdb on tests package

---
+++ tests/test_docdb.py
@@ -0,0 +1,15 @@
+import unittest
+import pynosql.columndb
+from unittest import mock
+
+
+class DocConnectionTest(unittest.TestCase):
+    pass
+
+
+class DocSessionTest(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
