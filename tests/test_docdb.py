a44556b6d94cee8ff65f22eddc922de299970830
Matteo Guadrini <matteo.guadrini@hotmail.it>
2021-07-22 15:17:31 +0200

    Added test_kvdb_exists_database method on DocConnectionTest class

--- tests/test_docdb.py
+++ tests/test_docdb.py
@@ -112,6 +112,15 @@ class DocConnectionTest(unittest.TestCase):
         self.assertEqual(myconn.connection, None)
         self.assertRaises(ConnectError, myconn.create_database, 'test_db')
 
+    def test_kvdb_exists_database(self):
+        myconn = MyDBConnection('mydocdb.local', 12345, username='admin', password='test')
+        myconn.connect()
+        self.assertEqual(myconn.connection, 'http://admin:test@mydocdb.local')
+        self.assertTrue(myconn.has_database('test_db'))
+        myconn.close()
+        self.assertEqual(myconn.connection, None)
+        self.assertRaises(ConnectError, myconn.create_database, 'test_db')
+
 
 class DocSessionTest(unittest.TestCase):
     pass
