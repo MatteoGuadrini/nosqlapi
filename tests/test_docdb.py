7866c1a718d4abba5b3b1703b1af03a7b862bf34
Matteo Guadrini <matteo.guadrini@hotmail.it>
2021-07-23 14:22:08 +0200

    Added find method on MyDBSession class

--- tests/test_docdb.py
+++ tests/test_docdb.py
@@ -210,6 +210,20 @@ class MyDBSession(nosqlapi.docdb.DocSession):
     def close(self):
         self.session = None
 
+    def find(self, selector):
+        if not self.session:
+            raise ConnectError('connect to a server before some request')
+        self.req.post = mock.MagicMock(return_value={'body': '{"_id": "5099803df3f4948bd2f98391"'
+                                                             '"name": "Matteo", "age": 35}',
+                                                     'status': 200,
+                                                     'header': '"Content-Type": [ "application/json" ]'})
+        ret = self.req.post(f"{self.session}/find", selector.build())
+        if ret.get('status') != 200:
+            raise SessionError(f'error: {ret.get("body")}, status: {ret.get("status")}')
+        return MyDBResponse(json.loads(ret.get('body')),
+                            ret['status'],
+                            ret['header'])
+
 
 class MyDBResponse(nosqlapi.docdb.DocResponse):
     pass
