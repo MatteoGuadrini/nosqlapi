687a2775afba5683f03a35b3084cdc33a908408a
Matteo Guadrini <matteo.guadrini@hotmail.it>
2021-07-27 09:34:31 +0200

    Added close method on MyDBConnection class

--- tests/test_graphdb.py
+++ tests/test_graphdb.py
@@ -1,13 +1,18 @@
 import unittest
 from unittest import mock
 import nosqlapi.graphdb
-
+from nosqlapi import (ConnectError)
 
 # Below classes is a simple emulation of Neo4j like database
 
 class MyDBConnection(nosqlapi.graphdb.GraphConnection):
     # Simulate http requests
     req = mock.Mock()
+
+    def close(self):
+        self.connection = None
+        if self.connection is not None:
+            raise ConnectError('Close connection error')
 
 
 class GraphConnectionTest(unittest.TestCase):
