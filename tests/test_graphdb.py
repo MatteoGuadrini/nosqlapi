e6368730c8a3fffa7c785b3daec4fc9cb14d643c
Matteo Guadrini <matteo.guadrini@hotmail.it>
2021-07-28 10:40:28 +0200

    Fix some MyDBConnection methods

--- tests/test_graphdb.py
+++ tests/test_graphdb.py
@@ -25,28 +25,42 @@ class MyDBConnection(nosqlapi.graphdb.GraphConnection):
         if self.username and self.password:
             url += f'{self.username}:{self.password}@'
         url += f'{self.host}:{self.port}'
-        self.req.get = mock.MagicMock(return_value={'body': 'server connected',
-                                                    'status': 200,
-                                                    'header': f'CONNECT TO {url}'
-                                                              f'WITH TIMEOUT 30'})
-        if self.req.get(url).get('status') != 200:
+        if self.database:
+            url += f'/{self.database}'
+        else:
+            raise ConnectError('database is mandatory')
+        self.req.post = mock.MagicMock(return_value={'body': 'server connected',
+                                                     'status': 200,
+                                                     'header': f'CONNECT TO {url}'
+                                                               f'WITH TIMEOUT 30'})
+        if self.req.post(url).get('status') != 200:
             raise ConnectError('server not respond')
         self.connection = url
-        # return MyDBSession(self.connection)
+        return MyDBSession(self.connection, self.database)
 
     def create_database(self, name, not_exists=False, replace=False, options=None):
         if not_exists and replace:
             raise DatabaseCreationError('IF NOT EXISTS and OR REPLACE parts of this command cannot be used together')
         if self.connection:
+            if not self.port:
+                self.port = 7474
+            scheme = 'bolt://'
+            url = f'{scheme}'
+            if self.username and self.password:
+                url += f'{self.username}:{self.password}@'
+            url += f'{self.host}:{self.port}'
             cypher = f'CREATE DATABASE {name}'
             if not_exists:
                 cypher = f' IF NOT EXISTS'
             elif replace:
                 cypher = f' OR REPLACE'
+            stm = {'statements': cypher}
+            if options:
+                stm['options'] = options
             self.req.post = mock.MagicMock(return_value={'body': '0 rows, System updates: 1',
                                                          'status': 200,
                                                          'header': cypher})
-            ret = self.req.post(f"{self.connection}", cypher, options)
+            ret = self.req.post(url, stm)
             if ret.get('status') != 200:
                 raise DatabaseCreationError(f'Database creation error: {ret.get("status")}')
             return MyDBResponse(ret['body'],
@@ -66,6 +80,13 @@ class MyDBConnection(nosqlapi.graphdb.GraphConnection):
 
     def delete_database(self, name, exists=False, dump=False, destroy=False):
         if self.connection:
+            if not self.port:
+                self.port = 7474
+            scheme = 'bolt://'
+            url = f'{scheme}'
+            if self.username and self.password:
+                url += f'{self.username}:{self.password}@'
+            url += f'{self.host}:{self.port}'
             cypher = f'DROP DATABASE {name}'
             if exists:
                 cypher += ' IF EXISTS'
@@ -75,10 +96,11 @@ class MyDBConnection(nosqlapi.graphdb.GraphConnection):
                 cypher += ' DUMP DATA'
             if destroy:
                 cypher += ' DESTROY DATA'
+            stm = {'statements': cypher}
             self.req.post = mock.MagicMock(return_value={'body': '0 rows, System updates: 1',
                                                          'status': 200,
                                                          'header': cypher})
-            ret = self.req.post(f"{self.connection}", cypher)
+            ret = self.req.post(url, stm)
             if ret.get('status') != 200:
                 raise DatabaseDeletionError(f'Database deletion error: {ret.get("status")}')
             return MyDBResponse(ret['body'],
@@ -89,11 +111,19 @@ class MyDBConnection(nosqlapi.graphdb.GraphConnection):
 
     def databases(self):
         if self.connection:
+            if not self.port:
+                self.port = 7474
+            scheme = 'bolt://'
+            url = f'{scheme}'
+            if self.username and self.password:
+                url += f'{self.username}:{self.password}@'
+            url += f'{self.host}:{self.port}'
             cypher = 'SHOW DATABASES'
+            stm = {'statements': cypher}
             self.req.get = mock.MagicMock(return_value={'body': '{"result": ["test_db", "db1", "db2"]}',
                                                         'status': 200,
                                                         'header': cypher})
-            ret = self.req.get(self.connection, cypher)
+            ret = self.req.get(url, stm)
             dbs = json.loads(ret.get('body'))
             if dbs['result']:
                 return MyDBResponse(dbs['result'],
