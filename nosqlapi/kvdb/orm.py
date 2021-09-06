09c47c6e1b6b8f811407a08ce6221bca05460a99
Matteo Guadrini <matteo.guadrini@hotmail.it>
2021-09-06 10:23:22 +0200

    Added orm module on kvdb package

---
+++ nosqlapi/kvdb/orm.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+# vim: se ts=4 et syn=python:
+
+# created by: matteo.guadrini
+# orm -- nosqlapi
+#
+#     Copyright (C) 2021 Matteo Guadrini <matteo.guadrini@hotmail.it>
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# region Classes
+class Item(dict):
+
+    def __init__(self, key, value=None):
+        super().__init__()
+        self._key = key
+        self._value = value
+
+
+# endregion
