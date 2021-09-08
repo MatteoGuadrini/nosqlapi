42274575b102d74d19eadf8cfb1e1f1fc1cb9a9e
Matteo Guadrini <matteo.guadrini@hotmail.it>
2021-09-08 11:58:17 +0200

    Added name property getter and setter on Keyspace class

--- nosqlapi/kvdb/orm.py
+++ nosqlapi/kvdb/orm.py
@@ -26,6 +26,14 @@ class Keyspace:
     def __init__(self, name):
         self._name = name
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
 
 class Item:
 
