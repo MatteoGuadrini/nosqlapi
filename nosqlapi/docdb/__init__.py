06a719d0db1973d121aa9eb018c478e82f1e7804
Matteo Guadrini <matteo.guadrini@hotmail.it>
2021-09-30 14:33:49 +0200

    Added docdb orm to imports from package

--- nosqlapi/docdb/__init__.py
+++ nosqlapi/docdb/__init__.py
@@ -23,3 +23,4 @@
 """Package document NOSQL database."""
 
 from nosqlapi.docdb.client import DocConnection, DocSelector, DocSession, DocResponse, DocBatch
+from nosqlapi.docdb.orm import Database, Document, Collection
