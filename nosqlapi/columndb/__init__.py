1906ee67d75d8db7c1da4f31f60b577a673bfad4
Matteo Guadrini <matteo.guadrini@hotmail.it>
2021-10-04 14:08:00 +0200

    Added Index object for imports on own package

--- nosqlapi/columndb/__init__.py
+++ nosqlapi/columndb/__init__.py
@@ -23,4 +23,4 @@
 """Package column NOSQL database."""
 
 from nosqlapi.columndb.client import ColumnConnection, ColumnSelector, ColumnSession, ColumnResponse, ColumnBatch
-from nosqlapi.columndb.orm import Keyspace, Table, Column
+from nosqlapi.columndb.orm import Keyspace, Table, Column, Index
