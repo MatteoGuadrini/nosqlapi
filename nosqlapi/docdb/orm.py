d4c62c8f99de864469204bc931a8898e69754a7e
Matteo Guadrini <matteo.guadrini@hotmail.it>
2021-10-04 12:05:36 +0200

    Added get/set/delitem methods on Index class

--- nosqlapi/docdb/orm.py
+++ nosqlapi/docdb/orm.py
@@ -134,5 +134,14 @@ class Index:
         self.data = {}
         self.data.update(data)
 
+    def __getitem__(self, item):
+        return self.data[item]
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
+
+    def __delitem__(self, key):
+        del self.data[key]
+
 
 # endregion
