15652e58158a04c32fb34c79dee537089f6083b1
Matteo Guadrini <matteo.guadrini@hotmail.it>
2021-10-13 11:12:37 +0200

    New orm stubs file

--- nosqlapi/common/orm.py
+++ nosqlapi/common/orm.py
@@ -20,6 +20,8 @@
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+"""Module that contains some ORM common objects."""
+
 # region Imports
 from decimal import Decimal as Dc
 from datetime import date
