507c2b14651b83f2c290a3263d675c3e377aadd5
Matteo Guadrini <matteo.guadrini@hotmail.it>
2021-10-14 10:17:23 +0200

    Added KVSelector class and its methods

--- nosqlapi/kvdb/client.pyi
+++ nosqlapi/kvdb/client.pyi
@@ -24,12 +24,12 @@ from ..common.core import Connection, Selector, Session, Response, Batch
 from typing import Any, Union
 
 
-class KVConnection:
+class KVConnection(Connection):
 
     return_data: Union[str, tuple, Response]
 
     def __init__(self, host=None, port=None, database=None, username=None, password=None, ssl=None, tls=None, cert=None,
-                 ca_cert=None, ca_bundle=None):
+                 ca_cert=None, ca_bundle=None) -> None:
         self.host: str = host
         self.port: int = port
         self.database: str = database
@@ -42,3 +42,16 @@ class KVConnection:
         self.ca_bundle: str = ca_bundle
         self.connection: Any = None
         self._return_data: Union[str, tuple, Response] = None
+
+
+class KVSelector(Selector):
+
+    def first_greater_or_equal(self, key: Any) -> Union[str, list, tuple]: ...
+
+    def first_greater_than(self, key: Any) -> Union[str, list, tuple]: ...
+
+    def last_less_or_equal(self, key: Any) -> Union[str, list, tuple]: ...
+
+    def last_less_than(self, key: Any) -> Union[str, list, tuple]: ...
+
+    def __str__(self) -> str: ...
