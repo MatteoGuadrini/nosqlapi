0139f6562197e79808aaf28c0d5e270dd9d5a2cd
Matteo Guadrini <matteo.guadrini@hotmail.it>
2021-10-19 11:29:34 +0200

    Added api decorator function

--- nosqlapi/common/utils.py
+++ nosqlapi/common/utils.py
@@ -21,3 +21,31 @@
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """Utils function and classes for any type of NOSQL database"""
+
+API_COMPLIANT_METHODS = ('close', 'connect', 'create_database', 'has_database', 'delete_database', 'databases',
+                         'show_database', 'get', 'insert', 'insert_many', 'update', 'update_many', 'delete', 'find',
+                         'grant', 'revoke', 'new_user', 'set_user', 'delete_user', 'add_index', 'add_index',
+                         'call', 'build', 'execute', 'link', 'detach')
+
+
+def api(**methods):
+    """Decorator function to transform standard classes into API compliant classes
+
+    :param methods: method names that you want to bind to the methods of API compliant classes
+    :return: class
+    """
+    def wrapped(cls):
+        """Wrap function to decorate standard class
+
+        :param cls: standard class you want to decorate
+        :return: object
+        """
+        for name, api_name in methods.items():
+            if api_name not in API_COMPLIANT_METHODS:
+                raise ValueError(f'{api_name} methods is not in API compliant methods')
+            if name in dir(cls):
+                setattr(cls, api_name, getattr(cls, name))
+                delattr(cls, name)
+        return cls
+
+    return wrapped
