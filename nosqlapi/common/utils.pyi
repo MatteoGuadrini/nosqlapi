32d5e50c2a8eabd0ecad4fbbd1fe2de3955710ca
Matteo Guadrini <matteo.guadrini@hotmail.it>
2021-10-25 09:53:01 +0200

    Added api function

--- nosqlapi/common/utils.pyi
+++ nosqlapi/common/utils.pyi
@@ -20,4 +20,9 @@
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from nosqlapi import Response, Batch, Connection
+from typing import Union
+
+def api(**methods: str) -> type: ...
+
 
