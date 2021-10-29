8ff3c53ab52a41cffb1851c52f7e5896ac73c600
Matteo Guadrini <matteo.guadrini@hotmail.it>
2021-10-29 11:57:34 +0200

    Added module docs string

--- nosqlapi/common/core.py
+++ nosqlapi/common/core.py
@@ -20,6 +20,8 @@
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+"""Module that contains the core objects."""
+
 # region imports
 from abc import ABC, abstractmethod
 from .exception import *
