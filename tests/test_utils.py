c83db58a32bfe116041cdf7c5f6b20aac056e469
Matteo Guadrini <matteo.guadrini@hotmail.it>
2022-02-18 10:59:03 +0100

    Add test_simple_response method on TestUtils class

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -98,6 +98,10 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(repr(resp), '<myapi MyDBResponse object>')
         nosqlapi.apply_vendor('nosqlapi')
 
+    def test_simple_response(self):
+        resp = nosqlapi.response('some data')
+        self.assertIsInstance(resp, nosqlapi.Response)
+
 
 if __name__ == '__main__':
     unittest.main()
