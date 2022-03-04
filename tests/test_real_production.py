14dd96e614644a6505beac10cf5c1d3808b44ff8
Matteo Guadrini <matteo.guadrini@hotmail.it>
2022-03-04 15:44:43 +0100

    Add test_cassandra_create_database_and_table function

--- tests/test_real_production.py
+++ tests/test_real_production.py
@@ -39,6 +39,34 @@ def test_cassandra_connect_database():
     # Close also Connection
     connection.close()
     assert connection.connected is False
+    return connection, session
+
+
+def test_cassandra_create_database_and_table():
+    """Test create database and table"""
+    connection, session = connect('mycolumndb.local', 'admin', 'password')
+    # Make a new database
+    connection.create_database("db1")
+    assert connection.has_database("db1")
+    assert "db1" in connection.databases()
+    # New session with new database db1
+    _, session_db1 = connect('mycolumndb.local', 'admin', 'password', 'db1')
+    # Create new table into database db1: without ORM objects
+    session_db1.create_table('table1', columns=[('id', int), ('name', str), ('age', int)], primary_key='id')
+    assert session_db1.item_count == 1
+    # Create new table into database db1: with ORM objects
+    ids = nosqlapi.columndb.Column('id', of_type=nosqlapi.Varint, primary_key=True)
+    assert isinstance(ids, nosqlapi.columndb.Column)
+    assert ids.primary_key
+    name = nosqlapi.columndb.Column('name', of_type=nosqlapi.Varchar)
+    age = nosqlapi.columndb.Column('age', of_type=nosqlapi.Varint)
+    table = nosqlapi.columndb.Table('table1')
+    table.primary_key = ids.name
+    session_db1.create_table('table1',
+                             columns=[ids, name, age],
+                             primary_key='id',
+                             not_exists=False)
+    assert session_db1.item_count == 1
 
 
 if __name__ == '__main__':
